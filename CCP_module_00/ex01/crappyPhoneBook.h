ef87dc3c1ee0e886c4c1bb5c6cf179a3dfa0df64
vping <kepovich98@bk.ru>
2021-08-17 19:31:51 +0300

    fixed 01

--- CCP_module_00/ex01/crappyPhoneBook.h
+++ CCP_module_00/ex01/crappyPhoneBook.h
@@ -19,35 +19,8 @@ class Contact
 	string	phoneNumber;
 	string	darkestSecret;
 public:
-	void addContact(int indexOfContact)
-	{
-		index = indexOfContact;
-		cout << "Put first name: ";
-		cin >> firstName;
-		cout << "Put last name: ";
-		cin >> lastName;
-		cout << "Put nickname: ";
-		cin >> nickName;
-		cout << "Put phone number: ";
-		cin >> phoneNumber;
-		cout << "Put darkest secret: ";
-		cin >> darkestSecret;
-		cout << "Contact added" << endl;
-	}
-	void printContact()
-	{
-		cout << "First name: " << firstName << endl;
-		cout << "Last name: " << lastName << endl;
-		cout << "Nickname: " << nickName << endl;
-		cout << "Phone number: " << phoneNumber << endl;
-		cout << "Darkest secret: " << darkestSecret << endl;
-	}
-	void printTable()
-	{
-		cout << std::right << std::setw(10) << index << "|";
-		cout << std::right << std::setw(10) << checkLength(firstName) << "|";
-		cout << std::right << std::setw(10) << checkLength(lastName) << "|";
-		cout << std::right << std::setw(10) << checkLength(nickName) << "|\n";
-	}
+	void addContact(int indexOfContact);
+	void printContact();
+	void printTable();
 };
 
