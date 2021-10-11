ef1e0ad4a2f36bae72bd6119cfd7b33667c774b5
vping <kepovich98@bk.ru>
2021-10-11 14:22:48 +0300

    go next

--- CPP_module_07/ex02/main.cpp
+++ CPP_module_07/ex02/main.cpp
@@ -1,19 +1,52 @@
 #include "Array.hpp"
 
-int main()
+#define MAX_VAL 750
+int main(int, char**)
 {
-	Array<int> array(8);
-	for (int i = 0; i < 8; ++i)
-		array[i] = i;
-	Array<int> copy1;
-	copy1 = array;
-	cout << copy1;
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
 	try
 	{
-		cout << array << std::endl;
+		numbers[-2] = 0;
 	}
-	catch (std::exception &e)
+	catch(const std::exception& e)
 	{
-		cout << "Error: " << e.what() << endl;
+		std::cerr << e.what() << '\n';
 	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete [] mirror;//
+	return 0;
 }
